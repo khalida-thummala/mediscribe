0b8456f74b1f31c0cdd4c062c5fcd0acc2d0d91f
khalida-thummala <khalidathummala38@gmail.com>
2026-05-01 06:38:28 +0530

    Remove old mediscribe folder

--- mediscribe/src/api/audit.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from './client'
-
-export const auditApi = {
-  list: (params = {}) =>
-    apiClient.get('/audit', { params }).then((r) => r.data),
-
-  exportCsv: () =>
-    apiClient.get('/audit/export', { responseType: 'blob' }).then((r) => r.data),
-}
